9a1c809a1ef49fabd721171f33dc4df4d37f5693
fpellet <git@florentpellet.com>
2020-04-26 14:59:14 +0200

    First exp

---
+++ runner/Program.fs
@@ -0,0 +1,143 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+open System.IO
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.Loader
+open FSharp.Compiler.SourceCodeServices
+
+let root = Path.GetFullPath("../../../../sample")
+let binDirectory = Path.Combine(root, "bin/Debug/netcoreapp3.1/")
+let source = Path.Combine(root, "Program.fs")
+let output = Path.Combine(binDirectory, "sample.dll")
+
+let dependencies = [
+    "C:\\Users\\flore\\.nuget\\packages\\expecto\\9.0.0\\lib\\netstandard2.0\\Expecto.dll"
+    "C:\\Users\\flore\\.nuget\\packages\\fsharp.core\\4.7.1\\lib\\netstandard2.0\\FSharp.Core.dll"
+]
+let arguments = [|
+    "fsc.exe"
+    "-o:" + output
+    "--nowin32manifest"
+    "-g"
+    "--debug:portable"
+    "--define:TRACE"
+    "--define:DEBUG"
+    "--define:NETCOREAPP"
+    "--define:NETCOREAPP3_1"
+    "--optimize-"
+    "--tailcalls-"
+    yield! dependencies |> Seq.map (fun path -> "-r:" + path)
+    "--nocopyfsharpcore"
+    "--simpleresolution"
+    source
+|]
+
+let searchEntryPoint (assembly:Assembly) =
+    if isNull assembly.EntryPoint
+    then
+        assembly.DefinedTypes
+        |> Seq.filter (fun t -> t.Name = "Program")
+        |> Seq.collect (fun t -> t.DeclaredMethods)
+        |> Seq.filter (fun m -> m.Name = "main")
+        |> Seq.head
+    else assembly.EntryPoint
+
+let callEntryPoint (entryPoint: MethodInfo) =
+    let args = [|""|]
+    entryPoint.Invoke(null, [|args :> obj|]) :?> int
+
+module DynamicAssemblies =
+    let compile (checker: FSharpChecker) =
+        let compileErrors, _, assembly = 
+            checker.CompileToDynamicAssembly(arguments, Some(stdout,stderr))
+            |> Async.RunSynchronously
+            
+        printfn "Error: %A" compileErrors
+        match assembly with
+        | None -> 1
+        | Some assembly ->
+            searchEntryPoint assembly
+            |> callEntryPoint
+
+module StaticAssemblies =
+    // https://docs.microsoft.com/en-us/dotnet/standard/assembly/unloadability
+    type AssemblyLoadContextWithUnloadSupport(path) =
+        inherit AssemblyLoadContext(isCollectible = true)
+        let resolver = AssemblyDependencyResolver(path)
+        
+        override x.Load(name: AssemblyName) =
+            let assemblyPath = resolver.ResolveAssemblyToPath(name)
+            if isNull assemblyPath |> not
+            then x.LoadFromAssemblyPath(assemblyPath)
+            else null
+
+    [<MethodImpl(MethodImplOptions.NoInlining)>]
+    let compileWithoutClean (checker: FSharpChecker) =
+        let compileErrors, _ = 
+            checker.Compile(arguments)
+            |> Async.RunSynchronously
+            
+        printfn "Error: %A" compileErrors
+        
+        let loader = AssemblyLoadContextWithUnloadSupport(binDirectory)
+        dependencies
+        |> Seq.iter (fun path -> loader.LoadFromAssemblyPath path |> ignore)
+        let assembly = loader.LoadFromAssemblyPath(output)
+        let weakAssembly = WeakReference(assembly)
+
+        let result =
+            searchEntryPoint assembly
+            |> callEntryPoint
+        
+        loader.Unload()
+        result, weakAssembly
+    
+    let rec waitClean (weak: WeakReference) =
+        GC.Collect()
+        GC.WaitForPendingFinalizers()
+        match weak.IsAlive with
+        | true -> waitClean weak
+        | false -> ()
+    
+    let compile (checker: FSharpChecker) =
+        let result, assembly = compileWithoutClean checker
+        
+        waitClean assembly
+        
+        result
+
+let checker = FSharpChecker.Create()
+
+let compile () =
+    let result = DynamicAssemblies.compile checker
+//    let result = StaticAssemblies.compile checker
+    printfn "Result: %i" result
+    
+let mutable lastRead = DateTime.MinValue
+
+let onChange (file: FileSystemEventArgs) =
+    printfn "%A %s" file.ChangeType file.FullPath
+    
+    let lastWriteTime = File.GetLastWriteTime(file.FullPath)
+    match lastWriteTime = lastRead with
+    | true -> printfn "<- ignored"
+    | false ->
+        lastRead <- lastWriteTime
+        compile ()
+
+[<EntryPoint>]
+let main argv =
+    use watcher = new FileSystemWatcher()
+    watcher.Path <- root
+    watcher.Filter <- "*.fs"
+    watcher.NotifyFilter <- NotifyFilters.LastWrite
+    watcher.Changed.Add onChange
+    watcher.EnableRaisingEvents <- true
+    
+    compile ()
+    
+    Console.ReadLine() |> ignore
+    0
---
+++ sample/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+open System.Threading.Tasks
+open Expecto
+
+let tests =
+  testList "A test group" [
+    test "one test" {
+      Expect.equal (2+2) 4 "2+2"
+    }
+
+    test "another test that fails" {
+      Expect.equal (3+3) 4 "3+3"
+    }
+
+    testAsync "this is an async test" {
+      let! x = async { return 4 }
+      Expect.equal x (2+2) "2+2"
+    }
+
+    testTask "this is a task test" {
+      let! n = Task.FromResult 2
+      Expect.equal n 2 "n=2"
+    }
+  ]
+  |> testLabel "samples"
+
+[<EntryPoint>]
+let main argv =
+    printfn "Start test"
+    tests
+    |> runTestsWithCLIArgs [] [||]
